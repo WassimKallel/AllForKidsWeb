177f67ef2b9a8967040dcabb742aaaaafcc17964
Wassim Kallel <wattouma@gmail.com>
2018-04-07 16:59:39 +0100

    added basic models

---
+++ Models/Blog/Tag.php
@@ -0,0 +1,10 @@
+<?php
+class Tag {
+    public $id, $name;
+
+    function __construct($id, $name){
+			$this->id=$id;
+			$this->name=$name;
+    }
+}
+
---
+++ Models/Forum/Topic.php
@@ -0,0 +1,10 @@
+<?php
+class Topic {
+    public $id, $name;
+
+    function __construct($id, $name){
+			$this->id=$id;
+			$this->name=$name;
+    }
+}
+
