eb73a8ac2b86fce0b4779575b92f88894a2969cf
Wassim Kallel <wattouma@gmail.com>
2018-04-29 02:05:16 +0100

    fixed downvoting

--- Views/Community/thread.php
+++ Views/Community/thread.php
@@ -76,7 +76,6 @@ $currentTopic = ForumController::getThreadTopic($thread);
                                                 <div class="white-bg cart-img">
                                                     <a class="media-link" href="#">
                                                         <img src="assets/img/cart/cart-1.png" alt="">
-                                                        
                                                     </a> 
                                                 </div>
                                                 <div style="text-align: center;">
@@ -98,7 +97,7 @@ $currentTopic = ForumController::getThreadTopic($thread);
                                                 <a id="upvote-<?= $post->id ?>" <?= ForumController::user_voted($post) ?  '' :  'onclick="upvote('. $post->id .')"' ?> > <i class="fa fa-caret-up"></i></a>
                                                 <p></p>
                                                 <p id="score-<?= $post->id ?>"><?= ForumController::count_score($post) ?></p>
-                                                <a id="downvote-<?= $post->id ?>" <?= ForumController::user_voted($post) ?  '' :  'onclick="upvote('. $post->id .')"' ?> > <i class="fa fa-caret-down"></i></a>
+                                                <a id="downvote-<?= $post->id ?>" <?= ForumController::user_voted($post) ?  '' :  'onclick="downvote('. $post->id .')"' ?> > <i class="fa fa-caret-down"></i></a>
                                             </td>
                                         </tr>
                                         <?php 
@@ -138,7 +137,7 @@ $currentTopic = ForumController::getThreadTopic($thread);
                                             } else {
                                         ?>
                                         <tr>
-                                            <td class="description" colspan="2">
+                                            <td class="description" colspan="3">
                                                 <h5>You need to <a href="#login-register" data-toggle="modal">Register/Login</a> in order to add a post</h5>    
                                                                                   
                                             </td>
